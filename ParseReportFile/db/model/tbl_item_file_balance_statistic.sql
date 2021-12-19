f8d4a3297e608a95fa1a34b9bd0e2b458dfcc6ee
Pavel Gnezdilov <pw095@fnbic.ru>
2021-12-19 12:18:15 +0300

    Исправить DDL таблицы tbl_item_file_balance_statistic

--- ParseReportFile/db/model/tbl_item_file_balance_statistic.sql
+++ ParseReportFile/db/model/tbl_item_file_balance_statistic.sql
@@ -1,9 +1,9 @@
-CREATE TABLE tbl_item_statistic
+CREATE TABLE tbl_item_file_balance_statistic
   (
     item_stat_id    INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    isf_id          INTEGER                           NOT NULL,
+    ifb_id          INTEGER                           NOT NULL,
     report_date     TEXT                              NOT NULL,
     item_stat_value INTEGER,
-    UNIQUE(isf_id, report_date),
-    FOREIGN KEY(isf_id) REFERENCES tbl_item_sheet_file(isf_id)
+    UNIQUE(ifb_id, report_date),
+    FOREIGN KEY(ifb_id) REFERENCES tbl_item_file_balance(ifb_id)
   );
