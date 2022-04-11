ebb7e8d843dd9191dc856db706c3144f43b47056
Pavel Gnezdilov <pw095@fnbic.ru>
2022-04-11 21:17:54 +0300

    Исправить обновление tbl_item_file_balance_2

--- ParseReportFile/db/persist/sql/insert/tbl_item_file_balance_2.sql
+++ ParseReportFile/db/persist/sql/insert/tbl_item_file_balance_2.sql
@@ -1,15 +1,29 @@
 UPDATE tbl_item_file_balance AS dest
    SET ifb_parent_id = (SELECT
                                ifb_parent.ifb_id
-                          FROM tmp_item_file_balance src
+                          FROM src.tmp_balance_item tmp
+                               JOIN
+                               tbl_emitter emitter
+                                   ON emitter.emitter_name = tmp.emitter_name
+                               JOIN
+                               tbl_file file
+                                   ON file.file_name = tmp.file_name
+                                  AND file.file_emitter_id = emitter.emitter_id
                                JOIN
                                tbl_item_file_balance ifb_parent
-                                   ON ifb_parent.ifb_file_id = src.ifb_file_id
-                                  AND ifb_parent.ifb_index = src.ifb_parent_index
-                         WHERE src.ifb_file_id = dest.ifb_file_id
-                           AND src.ifb_index = dest.ifb_index)
+                                   ON ifb_parent.ifb_file_id = file.file_id
+                                  AND ifb_parent.ifb_index = tmp.parent_item_index
+                         WHERE file.file_id = dest.ifb_file_id
+                           AND tmp.item_index = dest.ifb_index)
  WHERE EXISTS(SELECT
                      NULL
-                FROM tmp_item_file_balance src
-               WHERE src.ifb_file_id = dest.ifb_file_id
-                 AND src.ifb_index = dest.ifb_index)
+                FROM src.tmp_balance_item tmp
+                     JOIN
+                     tbl_emitter emitter
+                         ON emitter.emitter_name = tmp.emitter_name
+                     JOIN
+                     tbl_file file
+                         ON file.file_name = tmp.file_name
+                        AND file.file_emitter_id = emitter.emitter_id
+               WHERE file.file_id = dest.ifb_file_id
+                 AND tmp.item_index = dest.ifb_index)
